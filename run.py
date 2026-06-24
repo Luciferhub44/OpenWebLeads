832bea88f5a2edb81c5f83bc067ddf1415bf714e
LuciferHub44 <moltenwastelands@gmail.com>
2026-06-24 15:19:08 +0200

    Add Alembic migration command to run.py before starting FastAPI server

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import os
-import uvicorn
+import subprocess
 
+subprocess.run(["alembic", "upgrade", "head"], check=False)
+
+import uvicorn
 uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 7989)))
